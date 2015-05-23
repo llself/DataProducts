9f89f740bdf159eee7226972fc2faa2928c5e1d1
Leslie Self <leslieself@ymail.com>
2015-05-23 15:53:34 -0400

    Updated with help text documentation

--- ui.R
+++ ui.R
@@ -2,7 +2,14 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("Find the Movie with the Highest Average IMDB User Rating"),
   sidebarPanel(
-    numericInput('year', 'Enter a Year between 1903 and 2005', 1999, min = 1903, max = 2005, step = 1),
+    helpText("To find the movie with the highest average IMDB User Rating by genre and year,
+             just type in a year between 1903 and 2005 and then select one genre from the
+             list below and select Submit.  It will return the movie that you should go see!
+             Please Note:  Some movies are classified as being in multiple genre's so you may 
+             see that movie twice.  For example, I have defaulted it on year 1999 with a genre
+             of Comedy and I get Toy Story 2.  If I remove Comedy from the list and choose Animation
+             instead, I will get the same movie!  Have fun."),
+    numericInput('year', 'Enter a Year:', 1999, min = 1903, max = 2005, step = 1),
     checkboxGroupInput("genre", "Choose a Movie Genre (Note some movies are classified in multiple genres)",
                        c("Action" = "Action",
                          "Animation" = "Animation",
@@ -24,3 +31,6 @@ shinyUI(pageWithSidebar(
   )
 )
 )
+
+
+  
